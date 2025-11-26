257d02c82ba04fa17f77265ef2dc8eaa8d2042f5
mazharzaka <mazharmohamed566@gmail.com>
2025-11-26 07:06:51 +0200

    fix bcrypt and rebuild modules

--- utils/hash.js
+++ utils/hash.js
@@ -1,9 +1,10 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
+
 const saltRounds = 10;
 exports.hashPassword = async (password) => {
-    return await bcrypt.hash(password, saltRounds);
+  return await bcrypt.hash(password, saltRounds);
 };
 
 exports.comparePassword = async (password, hash) => {
-    return await bcrypt.compare(password, hash);
+  return await bcrypt.compare(password, hash);
 };
