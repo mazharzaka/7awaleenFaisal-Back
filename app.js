39a578cdf00973416cf20655a1b207ac21b2c400
mazharzaka <mazharmohamed566@gmail.com>
2025-11-27 19:52:02 +0200

    add 3000

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config();
 connectDB();
 app.use(
   cors({
-    origin: "https://7awaleen-faisal.vercel.app",
+    origin: ["https://7awaleen-faisal.vercel.app", "http://localhost:3000"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
