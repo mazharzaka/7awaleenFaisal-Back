c0ad430bdc43392f3ceb0887981df58aee3db94d
mazharzaka <mazharmohamed566@gmail.com>
2026-01-16 08:42:25 +0200

    add health endpoint

--- routers/user.router.js
+++ routers/user.router.js
@@ -6,5 +6,8 @@ const authMW = require("../middlewares/auth.middle");
 router.post("/", userController.createUser);
 router.post("/login", userController.login);
 router.get("/", authMW.verifyToken, userController.getUsers);
+router.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 module.exports = router;
